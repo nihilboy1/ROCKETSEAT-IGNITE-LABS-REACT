291e72a88949fede05505ffa20571b7b7398eb6d
Samuel Seve <samuelseve1@gmail.com>
2022-06-21 09:03:14 -0300

    Finalizando o setup da aplicação

--- eventplatform/src/App.tsx
+++ eventplatform/src/App.tsx
@@ -1,3 +1,28 @@
+import { gql, useQuery } from '@apollo/client'
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`
+
+interface Lesson {
+  id: string
+  title: string
+}
+
 export function App() {
-  return <h1 className="text-3xl font-bold text-red-600" >oi</h1>
+  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
+  console.log(data)
+
+  return (
+    <ul>
+      {data?.lessons.map(lesson => {
+        return <li key={lesson.id}>{lesson.title}</li>
+      })}
+    </ul>
+  )
 }
